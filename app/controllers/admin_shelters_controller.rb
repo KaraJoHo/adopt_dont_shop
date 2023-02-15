e6884251d633b08360c145e57d2c9bf78c507874
Matt Enyeart <menyeart1@gmail.com>
2023-02-14 18:04:25 -0700

    Add show method to admin shelters controller

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -3,4 +3,8 @@ class AdminSheltersController < ApplicationController
     @shelters = Shelter.order_by_reverse_alphabetical
     @pending_shelters = Shelter.list_pending
   end
+
+  def show
+    @shelter_name_and_city = Shelter.find_shelter_name_city(params[:id]).first
+  end
 end
