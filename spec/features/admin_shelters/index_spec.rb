6dee6f213d6b7684a4671d91f0caf22f66077e98
KaraJH <karas.jones@yahoo.com>
2023-02-14 17:33:32 -0700

    test: View of shelters order by pending alphabetically

--- spec/features/admin_shelters/index_spec.rb
+++ spec/features/admin_shelters/index_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "Admin Shelters Index" do
       shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
       shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
       pet1 = Pet.create!(adoptable: true, age: 46, breed: 'snapping', name: 'Shelly', shelter_id: shelter_1.id, )
-      app1 = Application.create!(name: 'Matt Smith', street_address: "1101 Main", city: "Denver", state: "CO", zipcode: 55555, description: "I like turtles!", status: "In Progress",  )
+      app1 = Application.create!(name: 'Matt Smith', street_address: "1101 Main", city: "Denver", state: "CO", zipcode: 55555, description: "I like turtles!", status: "Pending",  )
       petapplication1 = PetApplication.create!(pet_id: pet1.id, application_id: app1.id)
 
       visit "/admin/shelters"
@@ -33,6 +33,28 @@ RSpec.describe "Admin Shelters Index" do
       within('.pendingshelters') do
         expect(page).to have_content(shelter_1.name)
         expect(page).to_not have_content(shelter_2.name)
+        expect(page).to_not have_content(shelter_3.name)
+      end
+    end
+    it 'has pending applications listed alphabetically' do 
+      Shelter.destroy_all 
+      Application.destroy_all
+      shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+      shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+      pet1 = Pet.create!(adoptable: true, age: 46, breed: 'snapping', name: 'Shelly', shelter_id: shelter_1.id, )
+      app1 = Application.create!(name: 'Matt Smith', street_address: "1101 Main", city: "Denver", state: "CO", zipcode: 55555, description: "I like turtles!", status: "Pending")
+      petapplication1 = PetApplication.create!(pet_id: pet1.id, application_id: app1.id)
+      pet2 = Pet.create!(adoptable: true, age: 46, breed: 'snapping', name: 'Shelly', shelter_id: shelter_2.id, )
+      app2 = Application.create!(name: 'Smith Smith', street_address: "1101 Main", city: "Denver", state: "CO", zipcode: 55555, description: "I like turtles!", status: "Pending")
+      petapplication2 = PetApplication.create!(pet_id: pet2.id, application_id: app2.id)
+
+      visit "/admin/shelters"
+
+      expect(page).to have_content(shelter_1.name)
+
+      within(".pendingshelters") do 
+        expect(shelter_1.name).to appear_before(shelter_2.name)
         expect(page).to_not have_content(shelter_3.name)
       end
     end
